b8d0b3918b25ec20f6e72329dc6ac48d090568b1
Anthony Rathé <anthonyrathe@gmail.com>
2020-03-07 23:35:48 +0100

    .

--- src/main/main_daily.py
+++ src/main/main_daily.py
@@ -6,16 +6,16 @@ import pandas as pd
 
 
 # Generate screen for Europe (Yahoo Finance as source): fetch fundamentals, prices and process into screens
-#import src.main.EuropeDataScreen
+import src.main.EuropeDataScreen
 
 # Generate screen for USA (EDGAR as source): Fetch SEC fundamentals as long as the API limit stretches
 import src.main.fetch_EDGAR
 
 # Generate screen for USA (Yahoo Finance as source): fetch prices and process into screens
-#import src.main.DataProcessing
+import src.main.DataProcessing
 
 # Convert snapshots into Excel sheets
-#import src.main.ExportToSheets
+import src.main.ExportToSheets
 
 
 
